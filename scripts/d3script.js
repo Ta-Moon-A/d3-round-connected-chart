57d2c2a712f4a00cd877d2aa38292e07a72b24a0
ttkhlashidze <ttkhlashidze@bog.ge>
2017-10-12 18:59:53 +0400

    fit chart on boilerplate and change data struct

--- scripts/d3script.js
+++ scripts/d3script.js
@@ -17,7 +17,14 @@ function renderChart(params) {
     marginRight: 5,
     marginLeft: 5,
     container: 'body',
-    data: null
+    data: null,
+    hierarchyData: {
+      "name": "",
+      "children": []
+    },
+    cnfg: {
+      diameter: 600
+    }
   };
 
   /*############### IF EXISTS OVERWRITE ATTRIBUTES FROM PASSED PARAM  #######  */
@@ -43,19 +50,198 @@ function renderChart(params) {
       calc.chartWidth = attrs.svgWidth - attrs.marginRight - calc.chartLeftMargin;
       calc.chartHeight = attrs.svgHeight - attrs.marginBottom - calc.chartTopMargin;
 
+      var diameter = attrs.cnfg.diameter;
+      var radius = diameter / 2;
+      var innerRadius = radius - 70;
+
+
       //drawing containers
       var container = d3.select(this);
 
       //add svg
       var svg = container.patternify({ tag: 'svg', selector: 'svg-chart-container' })
-        .attr('width', attrs.svgWidth)
-        .attr('height', attrs.svgHeight)
+        .attr('width', diameter)
+        .attr('height', diameter)
       // .attr("viewBox", "0 0 " + attrs.svgWidth + " " + attrs.svgHeight)
       // .attr("preserveAspectRatio", "xMidYMid meet")
 
       //add container g element
       var chart = svg.patternify({ tag: 'g', selector: 'chart' })
-        .attr('transform', 'translate(' + (calc.chartLeftMargin) + ',' + calc.chartTopMargin + ')');
+        .attr('transform', 'translate(' + radius + ',' + radius + ')');
+
+
+      var cluster = d3.cluster()
+        .size([360, innerRadius])
+        .separation(function (a, b) { return (a.parent == b.parent ? 1 : a.parent.parent == b.parent.parent ? 2 : 4); });
+
+      var line = d3.line()
+        .x(xAccessor)
+        .y(yAccessor)
+        .curve(d3.curveBundle.beta(0.7));
+
+      var idToNode = {};
+
+      attrs.data.nodes.forEach(function (n) {
+        idToNode[n.id] = n;
+      });
+
+      attrs.data.links.forEach(function (e) {
+        e.source = idToNode[e.source];
+        e.target = idToNode[e.target];
+      });
+     
+      processData()
+      var tree = cluster(d3.hierarchy(attrs.hierarchyData));
+
+
+      debugger;
+      var leaves = tree.leaves();
+      var paths = attrs.data.links.map(function (l) {
+        var source = leaves.filter(function (d) { return d.data === l.source; })[0];
+        var target = leaves.filter(function (d) { return d.data === l.target; })[0];
+        return source.path(target);
+      });
+
+      var link = chart.selectAll('.link')
+        .data(paths)
+        .enter().append('path')
+        .attr('class', 'link')
+        .attr('d', function (d) { return line(d) })
+        .on('mouseover', function (l) {
+          link
+            .style('stroke', null)
+            .style('stroke-opacity', null);
+          
+          d3.select(this)
+            .style('stroke', '#d62333')
+            .style('stroke-opacity', 1);
+          
+          node.selectAll('circle')
+            .style('fill', null);
+
+          node.filter(function (n) { return n === l[0] || n === l[l.length - 1]; })
+            .selectAll('circle')
+            .style('fill', 'black');
+        })
+        .on('mouseout', function (d) {
+          link
+            .style('stroke', null)
+            .style('stroke-opacity', null);
+          node.selectAll('circle')
+            .style('fill', null);
+        });
+
+      var node = chart.selectAll('.node')
+        .data(tree.leaves())
+        .enter().append('g')
+        .attr('class', 'node')
+        .attr('transform', function (d) { return 'translate(' + xAccessor(d) + ',' + yAccessor(d) + ')'; })
+        .on('mouseover', function (d) {
+          node.style('fill', null);
+          d3.select(this).selectAll('circle').style('fill', 'black');
+          var nodesToHighlight = paths.map(function (e) { return e[0] === d ? e[e.length - 1] : e[e.length - 1] === d ? e[0] : 0 })
+            .filter(function (d) { return d; });
+          node.filter(function (d) { return nodesToHighlight.indexOf(d) >= 0; })
+            .selectAll('circle')
+            .style('fill', '#555');
+          link
+            .style('stroke-opacity', function (link_d) {
+              return link_d[0] === d | link_d[link_d.length - 1] === d ? 1 : null;
+            })
+            .style('stroke', function (link_d) {
+              return link_d[0] === d | link_d[link_d.length - 1] === d ? '#d62333' : null;
+            });
+        })
+        .on('mouseout', function (d) {
+          link
+            .style('stroke-opacity', null)
+            .style('stroke', null);
+          node.selectAll('circle')
+            .style('fill', null);
+        });
+
+      node.append('circle').attr('r', 4)
+        .append('title')
+        .text(function (d) { return d.data.name; });
+
+      node.append('text')
+        .attr('dy', '0.32em')
+        .attr('x', function (d) { return d.x < 180 ? 6 : -6; })
+        .style('text-anchor', function (d) { return d.x < 180 ? 'start' : 'end'; })
+        .attr('transform', function (d) { return 'rotate(' + (d.x < 180 ? d.x - 90 : d.x + 90) + ')'; })
+        .text(function (d) {return d.data.name; });
+
+
+      function processData() {
+        var nested_data = d3.nest()
+          .key(function (d) { return d.group; })
+          .entries(attrs.data.nodes);
+
+        var mappedData = nested_data.map(function (d) {
+          return {
+            "name": d.key,
+            "children": d.values
+          }
+        })
+        attrs.hierarchyData.children = mappedData;
+        console.log(attrs.hierarchyData);
+      }
+
+
+      function chapterCompare(aChaps, bChaps) {
+        if (aChaps[0] != bChaps[0])
+          return bChaps[0] - aChaps[0];
+        else if (aChaps[1] != bChaps[0])
+          return bChaps[1] - aChaps[1];
+        else if (aChaps[2] != bChaps[2])
+          return bChaps[2] - aChaps[2];
+        return 0;
+      }
+
+
+      function chapterHierarchy(characters) {
+        var hierarchy = {
+          root: { name: 'root', children: [] }
+        };
+
+        characters.forEach(function (c) {
+          var chapter = c.firstChapter;
+
+          var book = c.firstChapter.substring(0, c.firstChapter.lastIndexOf('.'));
+
+          var volume = book.substring(0, book.lastIndexOf('.'));
+
+          if (!hierarchy[volume]) {
+            hierarchy[volume] = { name: volume, children: [], parent: hierarchy['root'] };
+            hierarchy['root'].children.push(hierarchy[volume]);
+          }
+          if (!hierarchy[book]) {
+            hierarchy[book] = { name: book, children: [], parent: hierarchy[volume] };
+            hierarchy[volume].children.push(hierarchy[book]);
+          }
+          if (!hierarchy[chapter]) {
+            hierarchy[chapter] = { name: chapter, children: [], parent: hierarchy[book] };
+            hierarchy[book].children.push(hierarchy[chapter]);
+          }
+
+          c.parent = hierarchy[chapter];
+          hierarchy[chapter].children.push(c);
+        });
+        debugger;
+        return hierarchy['root'];
+      }
+
+
+      function xAccessor(d) {
+        var angle = (d.x - 90) / 180 * Math.PI, radius = d.y;
+        return radius * Math.cos(angle);
+      }
+      function yAccessor(d) {
+        var angle = (d.x - 90) / 180 * Math.PI, radius = d.y;
+        return radius * Math.sin(angle);
+      }
+
+
 
       // smoothly handle data updating
       updateData = function () {
